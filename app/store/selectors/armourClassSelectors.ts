3140172c7c3c0c6aa8901a247e4980f1ca7fe87f
Andre Lemay <lemay.andre@gmail.com>
2018-12-21 00:22:55 -0500

    incorporated maxDex into ac calculation

--- app/store/selectors/armourClassSelectors.ts
+++ app/store/selectors/armourClassSelectors.ts
@@ -8,18 +8,19 @@ const getArmourAC = (state: CharacterSheetState) => state.armour.ac
 const getShieldAC = (state: CharacterSheetState) => state.shield.ac
 const getNatAC = (state: CharacterSheetState) => state.armourClass.natArmour
 const getSizeModifier = (state: CharacterSheetState) => state.character.size.acModifier
+const getMaxDex = (state: CharacterSheetState) => state.armour.maxDex
 
 const getArmourCheckPenalty = (state: CharacterSheetState) => state.armour.checkPenalty
 const getShieldCheckPenalty = (state: CharacterSheetState) => state.shield.checkPenalty
 
-const calcAC = (dex: number, dodge: number, deflect: number, armour: number, shield: number, nat: number, size: number) => {
-    return 10 + dex + dodge + deflect + armour + shield + nat + size
+const calcAC = (dex: number, dodge: number, deflect: number, armour: number, shield: number, nat: number, size: number, maxDex: number) => {
+    return 10 + (typeof(maxDex) === "number" ? Math.min(dex, maxDex) : dex) + dodge + deflect + armour + shield + nat + size
 }
 const calcFlatFooted = (deflect: number, armour: number, shield: number, nat: number, size: number) => {
-    return calcAC(0, 0, deflect, armour, shield, nat, size)
+    return calcAC(0, 0, deflect, armour, shield, nat, size, 0)
 }
-const calcTouch = (dex: number, dodge: number, deflect: number, size: number) => {
-    return calcAC(dex, dodge, deflect, 0, 0, 0, size)
+const calcTouch = (dex: number, dodge: number, deflect: number, size: number, maxDex: number) => {
+    return calcAC(dex, dodge, deflect, 0, 0, 0, size, maxDex)
 }
 
 export const getArmourPenalty = createSelector([getArmourCheckPenalty, getShieldCheckPenalty], (armour, shield) => armour + shield)
@@ -30,6 +31,6 @@ export const getArmourMod = createSelector([getArmourAC], (armour) => armour)
 export const getShieldMod = createSelector([getShieldAC], (shield) => shield)
 export const getNatMod = createSelector([getNatAC], (nat) => nat)
 export const getSizeMod = createSelector([getSizeModifier], (size) => size)
-export const getAC = createSelector([getDexterityBonus, getDodgeMod, getDeflectionMod, getArmourMod, getShieldMod, getNatMod, getSizeMod], calcAC)
+export const getAC = createSelector([getDexterityBonus, getDodgeMod, getDeflectionMod, getArmourMod, getShieldMod, getNatMod, getSizeMod, getMaxDex], calcAC)
 export const getFlatFootedAC = createSelector([getDeflectionMod, getArmourMod, getShieldMod, getNatMod, getSizeMod], calcFlatFooted)
-export const getTouchAC = createSelector([getDexterityBonus, getDodgeMod, getDeflectionMod, getSizeMod], calcTouch)
+export const getTouchAC = createSelector([getDexterityBonus, getDodgeMod, getDeflectionMod, getSizeMod, getMaxDex], calcTouch)
