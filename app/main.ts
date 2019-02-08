0d826d049b916e20ab2ba41569e9f3ea1e796a45
Andre Lemay <lemay.andre@gmail.com>
2019-02-08 12:24:41 -0500

    Added electron-builder for windows packaging. Removed devtools and other dev-only stuff

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
-import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from "electron-devtools-installer";
+//import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from "electron-devtools-installer";
 
 let mainWindow: Electron.BrowserWindow;
 
@@ -13,11 +13,11 @@ function createWindow() {
         }
     });
 
-    installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS]).then((name) => {
-        console.log(`Added Extension:  ${name}`);
-    }).catch((err) => {
-        console.log('An error occurred: ', err)
-    });
+    // installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS]).then((name) => {
+    //     console.log(`Added Extension:  ${name}`);
+    // }).catch((err) => {
+    //     console.log('An error occurred: ', err)
+    // });
 
     mainWindow.loadFile(path.join(__dirname, "../views/main.html"));
 
--- app/renderer.tsx
+++ app/renderer.tsx
@@ -2,26 +2,26 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from "./App"
 import * as $ from "jquery"
-import { remote } from "electron";
+//import { remote } from "electron";
 
 /* Remove this stuff when finished, just a debug menu */
-let rightClickPosition = null
-const menu = new remote.Menu()
-const menuItem = new remote.MenuItem({
-    label: 'Inspect Element',
-    click: () => {
-        remote.getCurrentWindow().webContents.inspectElement(rightClickPosition.x, rightClickPosition.y)
-    }
-})
-menu.append(menuItem)
+// let rightClickPosition = null
+// const menu = new remote.Menu()
+// const menuItem = new remote.MenuItem({
+//     label: 'Inspect Element',
+//     click: () => {
+//         remote.getCurrentWindow().webContents.inspectElement(rightClickPosition.x, rightClickPosition.y)
+//     }
+// })
+// menu.append(menuItem)
 
-window.addEventListener('contextmenu', (e) => {
-    e.preventDefault()
-    rightClickPosition = { x: e.x, y: e.y }
-    menu.popup({
-        window: remote.getCurrentWindow()
-    })
-}, false)
+// window.addEventListener('contextmenu', (e) => {
+//     e.preventDefault()
+//     rightClickPosition = { x: e.x, y: e.y }
+//     menu.popup({
+//         window: remote.getCurrentWindow()
+//     })
+// }, false)
 /* --------------------------------------------------- */
 
 window.onload = () => {
