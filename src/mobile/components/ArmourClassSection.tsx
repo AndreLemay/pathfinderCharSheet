4acffd6ae703f2f9d4254c8bc391c0f26871fa7c
Andre Lemay <lemay.andre@gmail.com>
2019-07-08 15:26:04 -0400

    Fixed styling for ac and combat maneuver sections

--- src/mobile/components/ArmourClassSection.tsx
+++ src/mobile/components/ArmourClassSection.tsx
@@ -7,97 +7,90 @@ import OutputField from "./common/OutputField";
 
 export default function ArmourClassSection(props: ArmourClassProps) {
     return (
-        <View style={[Styles.container]}>
+        <View>
             <SectionHeader label="Armour Class" />
             <View style={[Styles.row]}>
                 <OutputField label="Total"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.ac} />
                 <OutputField label="Base"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={10} />
                 <OutputField label="DEX"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.dexBonus} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Dodge"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.dodgeBonus} />
                 <OutputField label="Deflection"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.deflectionBonus} />
                 <OutputField label="Armour"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.armourBonus} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Shield"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.shieldBonus} />
                 <OutputField label="Natural"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.natBonus} />
                 <OutputField label="Size"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.sizeBonus} />
             </View>
             <SectionHeader label="Flat-Footed" />
             <View style={[Styles.row]}>
                 <OutputField label="Total"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.ac} />
                 <OutputField label="Base"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={10} />
-                <View style={[Styles.col]} />
-            </View>
-            <View style={[Styles.row]}>
-                <View style={[Styles.col]} />
                 <OutputField label="Deflection"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.deflectionBonus} />
-                <OutputField label="Armour"
-                    style={[Styles.col]}
-                    value={props.armourBonus} />
             </View>
             <View style={[Styles.row]}>
+                <OutputField label="Armour"
+                    containerStyle={[Styles.col]}
+                    value={props.armourBonus} />
                 <OutputField label="Shield"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.shieldBonus} />
                 <OutputField label="Natural"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.natBonus} />
+            </View>
+            <View style={[Styles.row]}>
                 <OutputField label="Size"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.sizeBonus} />
             </View>
             <SectionHeader label="Touch" />
             <View style={[Styles.row]}>
                 <OutputField label="Total"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.ac} />
                 <OutputField label="Base"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={10} />
                 <OutputField label="DEX"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.dexBonus} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Dodge"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.dodgeBonus} />
                 <OutputField label="Deflection"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.deflectionBonus} />
-                <View style={[Styles.col]} />
-            </View>
-            <View style={[Styles.row]}>
-                <View style={[Styles.col]} />
-                <View style={[Styles.col]} />
                 <OutputField label="Size"
-                    style={[Styles.col]}
+                    containerStyle={[Styles.col]}
                     value={props.sizeBonus} />
             </View>
         </View>
--- src/mobile/components/CombatManeuverSection.tsx
+++ src/mobile/components/CombatManeuverSection.tsx
@@ -7,85 +7,85 @@ import OutputField from "./common/OutputField";
 
 export default function CombatManeuverSection(props: CombatManeuvreProps) {
     return (
-        <View style={Styles.container}>
+        <View>
             <SectionHeader label="Combat Maneuvers" />
             <View style={[Styles.row]}>
                 <OutputField label="CMB"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.cmb} />
                 <OutputField label="STR"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.strBonus} />
                 <OutputField label="BAB"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.bab} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Size"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.sizeMod} />
                 <OutputField label="Misc"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.miscCMB} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="CMD"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.cmd} />
                 <OutputField label="Base"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={10} />
                 <OutputField label="STR"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.strBonus} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="DEX"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.dexBonus} />
                 <OutputField label="Dodge"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.dodgeMod} />
                 <OutputField label="Deflection"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.deflectionMod} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="BAB"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.bab} />
                 <OutputField label="Size"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.sizeMod} />
                 <OutputField label="Misc"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.miscCMD} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Flat-Footed"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.flatFooted} />
                 <OutputField label="Base"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={10} />
                 <OutputField label="STR"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.strBonus} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Deflection"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.deflectionMod} />
                 <OutputField label="BAB"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.bab} />
                 <OutputField label="Size"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.sizeMod} />
             </View>
             <View style={[Styles.row]}>
                 <OutputField label="Misc"
-                    style={[Styles.row]}
+                    containerStyle={[Styles.row]}
                     value={props.miscCMD} />
             </View>
         </View>
